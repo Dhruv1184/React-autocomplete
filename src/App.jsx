d95a5739dbe4936375cba259f0961d458280ebc1
Dhruv1184 <dhruvkhadelwal1184@gmail.com>
2023-12-16 09:23:47 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import Data from './resource/countryData.json'
+import './App.css'
+
+function App() {
+  const [search,setSearch] = useState('')
+  const [visible,setVisible] = useState(true)
+  
+  function Escape(e){
+    // console.log(e.keyCode)
+    if (e.keyCode == 27){
+      setVisible(!visible)
+    }
+    console.log('Escape')
+  }
+  
+  function Search(e){
+  setSearch(e.target.value)
+  
+  }  
+// console.log(Data)
+  return (
+
+    <>
+    <h1>Search</h1>
+      <input type="text" onChange={(e)=>Search(e)} onKeyDown={(e)=>Escape(e)} placeholder='Search here'/>
+      <div>
+        {Data
+        .filter((item)=>item.name.toLowerCase().includes(search))
+        .map((items)=>( 
+          <p key={items.code}>{visible?items.name:null}</p>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
